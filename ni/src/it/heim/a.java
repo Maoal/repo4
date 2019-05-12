1899bd5ed10f240756f53a155b87a27b43999f68
admin <admin@163.com>
2019-05-12 20:10:55 +0800

    日期2

--- ni/src/it/heim/a.java
+++ ni/src/it/heim/a.java
@@ -4,8 +4,7 @@ public class a {
     public static void main(String[] args) {
         System.out.println("你好" +"");
         System.out.println("你好" +"");
-        System.out.println("你好" +"");
-        System.out.println("你好" +"");
+
     }
 
 }
